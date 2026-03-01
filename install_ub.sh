4383253523aecfb73459936c91bfb8d615334c1b
Jan Spindler <Jan Spindler>
2026-03-01 17:24:58 +0000

    added install script

---
+++ install_ub.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
+export TORCH_CUDA_ARCH_LIST="8.0"
+pip install --no-build-isolation git+https://github.com/nerfstudio-project/gsplat.git
+pip install --no-build-isolation git+https://github.com/facebookresearch/pytorch3d.git
+pip install imageio numba omegaconf open3d opencv-python scipy smplx scikit-image tensorboardx tensorboard \
+    trimesh websockets torchmetrics websocket-client dearpygui plyfile torch_pca lpips
